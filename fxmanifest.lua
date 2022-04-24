65ea29bced3776d6a45e91aae5faf606d51bdaa0
Angelo G <99841150+AngeloGraniello@users.noreply.github.com>
2022-04-24 05:11:42 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+client_scripts {
+	"coords.lua",
+	"configs/config.lua"
+}
+
+ui_page 'nui/index.html'
+
+files {
+	'nui/app.js',
+	'nui/index.html',
+	'nui/image/logo.png',
+	'nui/sounds/click.mp3',
+	'nui/style.css',
+}
+
