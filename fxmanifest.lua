fca1ea2e325b15a6880c888ccfdf0db9f6a3807b
Angelo G <99841150+AngeloGraniello@users.noreply.github.com>
2022-04-25 18:57:16 +0200

    Update fxmanifest.lua
    Added the version and description of the script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,17 @@
-fx_version 'adamant'
-games { 'gta5' }
+fx_version 'adamant' --Don't modify this
+games { 'gta5' } --Don't modify this
+
+description 'With this advanced coordinate system you can copy any type of structure containing coordinates to make your work easier.'
+version '1.0.0' --Don't modify this
 
 client_scripts {
-	"coords.lua",
-	"configs/config.lua"
+	"coords.lua", --Don't modify this
+	"configs/config.lua" --Don't modify this
 }
 
-ui_page 'nui/index.html'
+ui_page 'nui/index.html' --Don't modify this 
 
-files {
+files { --Don't modify this
 	'nui/app.js',
 	'nui/index.html',
 	'nui/image/logo.png',
